be9c8d80b01ad7f49f8778d74f53bacde99e076d
Man Dangi <mandangi773@gmail.com>
2026-07-24 21:38:49 +0530

    Time: 0 ms (100%), Space: 8.1 MB (82.92%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
-        int count = 0;
-        while(n != 0){
-            count += (n%2);
-            n >>= 1;
+    int count = 0;
+    while(n != 0){
+    count += (n%2); 
+    n >>= 1;
         }
         return count;
     }

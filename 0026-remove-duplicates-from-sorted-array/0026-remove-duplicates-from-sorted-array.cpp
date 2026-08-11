655e7f863bfd49c2b5ea0bac09a2c98d48d34ebf
Man Dangi <mandangi773@gmail.com>
2026-08-11 15:53:55 +0530

    Time: 4 ms (7.83%), Space: 23.5 MB (5.72%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -2,27 +2,20 @@ class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
 
+        set<int> s;
+
+     
+        for (int x : nums) {
+            s.insert(x);
+        }
+
         int i = 0;
-        int j = 1;
 
-        vector<int> temp;
-
-        temp.push_back(nums[0]);   // Pehla element add karo
-
-        while (j < nums.size()) {
-
-            if (nums[i] < nums[j]) {
-                temp.push_back(nums[j]);
-                i = j;
-            }
-
-            j++;
+        for (int x : s) {
+            nums[i] = x;
+            i++;
         }
 
-        for (int k = 0; k < temp.size(); k++) {
-            nums[k] = temp[k];
-        }
-
-        return temp.size();
+        return i;
     }
 };

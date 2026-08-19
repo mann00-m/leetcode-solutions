97341d416fbd10661a582d3ceeebb5c21ce827cd
Man Dangi <mandangi773@gmail.com>
2026-08-20 00:41:28 +0530

    Time: 0 ms (100%), Space: 50.2 MB (34.51%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
@@ -4,7 +4,7 @@ public:
         int left=0;
         int maxlen=0;
         for(int i=0;i<nums.size();i++){
-            if(nums[i]==0){
+            if(nums[i]!=1){
                 left=i+1;
             }
             maxlen=max(maxlen,i-left+1);

8192f42518951a7cbad93a525a4b1b7443d1fbaa
Man Dangi <mandangi773@gmail.com>
2026-09-08 11:29:34 +0530

    Time: 3 ms (13.67%), Space: 8.5 MB (84.49%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.cpp
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
-    long long countCommas(int n) {
-        long long count = 0;
+    int countCommas(int n) {
+        long long count=0;
 
-        for (int i = 999; i <= n; i++) {
-            if (i >= 1000) {
+        for(int i=999;i<=n;i++){
+            if(i>=1000){
                 count++;
             }
         }
-
         return count;
     }
 };
